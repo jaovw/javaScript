3689904d4ca66bd03c062dfbbe88fe2163c369af
joao.victor <joao.victor@superfestval.com.br>
2022-05-09 09:44:05 -0300

    Express

---
+++ Node/express/server/server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+
+//      http://meusite.com/  >>  GET  >>  ENTREGUE A PÁGINA " / "
+//      http://meusite.com/sobre  >>  GET  >>  ENTREGUE A PÁGINA " /SOBRE "
+//      http://meusite.com/contato  >>  GET  >>  ENTREGUE A PÁGINA " /CONTATO "
+
+// app.get('/', (req, res) => {    //  REQUISIÇÃO (REQUEST) E REPOSTA (RESPONSE) 
+//     res.send('Hello World')
+// })
+
+app.get('/', (req, res) => {
+    res.send(`
+                <form action= "/" method = "POST" >
+                    Nome: <input type = "text" name = "nome" ></input>
+                    <button>Enviar</button>
+                </form>
+     `)
+})
+
+app.post('/', (req, res) => {
+    res.send('Formulário recebido.')
+})
+
+app.get('/contato', (req, res) => {
+    res.send('Página para contato')
+})
+
+app.listen(3000, () => {
+    console.log('Acesse http://localhost:3000')
+})
